469b4aabac88a624be3d5f530fbeabe58d2cb9ac
Zubair-Rajoot <zubair.36aug24webgpt@gmail.com>
2025-03-26 02:42:53 +0500

    Fixed Vercel deployment settings

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Ensure the output directory is correctly set
+    outDir: 'dist' , // Ensure Vite outputs to "dist"
+    ignoreDuringBuild: true
   }
 });
-
